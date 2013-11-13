3c744d318b33a7ecc85e1bda69b74eceb0e7c4cc
Marijn Haverbeke <marijnh@gmail.com>
2013-11-13 14:29:03 +0100

    [doc toc script] More accurate active link highlighting

--- doc/activebookmark.js
+++ doc/activebookmark.js
@@ -39,4 +39,14 @@
 
   window.addEventListener("scroll", updateSoon);
   window.addEventListener("load", updateSoon);
+  window.addEventListener("hashchange", function() {
+    setTimeout(function() {
+      var hash = document.location.hash, found = null, m;
+      var marks = document.getElementById("nav").getElementsByTagName("a");
+      for (var i = 0; i < marks.length; i++)
+        if ((m = marks[i].href.match(/(#.*)/)) && m[1] == hash) { found = i; break; }
+      if (found != null) for (var i = 0; i < marks.length; i++)
+        marks[i].className = i == found ? "active" : "";
+    }, 300);
+  });
 })();
